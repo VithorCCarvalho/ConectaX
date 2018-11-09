ad538084cc61165b5e3c39fc0e7a49745e9cbc28
João Vithor <j.vithor13@gmail.com>
2018-11-09 11:30:37 -0200

    Update index.php

--- index.php
+++ index.php
@@ -83,7 +83,7 @@
 			</div>
 		</div>
 	</header>
-	
+
 	<div class="gtco-section border-bottom">
 		<div class="gtco-container">
 			<div class="row">
@@ -137,55 +137,130 @@
 		<div class="gtco-container">
 			<div class="row">
 				<div class="col-md-8 col-md-offset-2 text-center gtco-heading">
-					<h2>Confira alguns planos</h2>
-					<p></p>
+					<h2>Oi Total Play</h2>
+					<p>Internet de alta velocidade e milhares de filmes e séries com o Coleção OI</p>
 				</div>
 			</div>
 			<div class="row">
 				<div class="col-md-4">
 					<div class="price-box">
-						<h2 class="pricing-plan">TOTAL PLAY</h2>
+						<h2 class="pricing-plan">Play Básico</h2>
 						<div class="price">R$ 119,90<small>/mês</small></div>
-						<p>OI Total Play Básico</p>
-						<hr>
+						<p></p>
 						<ul class="pricing-info">
-							<li>Internet de 15 MB</li>
-							<li>Filmes e Séries Online</li>
-							<li>Telefone Fixo Ilimitado</li>
-							<li>Wifi Grátis Dentro e Fora de Casa</li>
+							<li><div class="pricing-info-icon"><img width="30" src="images/banda-larga.svg"></div>Internet 15 MB</li>
+							<li><div class="pricing-info-icon"><img width="32" src="images/fixo.svg"></div>Telefone Fixo Ilimitado</li>
+							<li><div class="pricing-info-icon"><img width="26" src="images/internet-celular.svg"></div>OI WIFI Fora de casa</li>
+							
+							<li style="margin: 0 auto; text-align: center;"><img style="width: 120px; padding-top: 10px;" src="images/colecaooi.png"></li>
+						</ul>
+						<a href="#" class="btn btn-primary btn-sm">Aderir</a>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="price-box popular">
+						<div class="popular-text">Popular</div>
+						<h2 class="pricing-plan">Play Intermediário</h2>
+						<div class="price">R$ 139,90<small>/mês</small></div>
+						<p></p>
+						<ul class="pricing-info">
+							<li><div class="pricing-info-icon"><img width="30" src="images/banda-larga.svg"></div>Internet 15 MB</li>
+							<li><div class="pricing-info-icon"><img width="32" src="images/fixo.svg"></div>Telefone Fixo Ilimitado</li>
+							<li><div class="pricing-info-icon"><img width="26" src="images/internet-celular.svg"></div>OI WIFI Fora de casa</li>
+							
+							<li style="margin: 0 auto; text-align: center;"><img style="width: 120px; padding-top: 10px;" src="images/playtop.png"></li>
 						</ul>
 						<a href="#" class="btn btn-primary btn-sm">Aderir</a>
 					</div>
 				</div>
 				<div class="col-md-4">
 					<div class="price-box">
-						<h2 class="pricing-plan">TOTAL RESIDENCIAL</h2>
-						<div class="price">R$ 204,90<small>/mês</small></div>
-						<p>Oi Total Residencial Intermediário</p>
-						<hr>
+						<h2 class="pricing-plan">Play Top</h2>
+						<div class="price">R$ 164,90<small>/mês	</small></div>
+						<p></p>
 						<ul class="pricing-info">
-							<li>Internet de 15 MB</li>
-							<li>TV com 162 canais 56 HD</li>
-							<li>Telefone Fixo Ilimitado</li>
-							<li>Wifi Grátis Dentro e Fora de Casas</li>
+							<li><div class="pricing-info-icon"><img width="30" src="images/banda-larga.svg"></div>Internet 15 MB</li>
+							<li><div class="pricing-info-icon"><img width="32" src="images/fixo.svg"></div>Telefone Fixo Ilimitado</li>
+							<li><div class="pricing-info-icon"><img width="26" src="images/internet-celular.svg"></div>OI WIFI Fora de casa</li>
+							
+							<li style="margin: 0 auto; text-align: center;"><img style="width: 120px; padding-top: 10px;" src="images/playtop.png"></li>
 						</ul>
 						<a href="#" class="btn btn-primary btn-sm">Aderir</a>
 					</div>
 				</div>
-				<div class="col-md-4">
-					<div class="price-box">
-						<h2 class="pricing-plan">TOTAL CONECTADO</h2>
-						<div class="price">R$ 164,90<small>/mês</small></div>
-						<p>Oi Total Conectado Light</p>
-						<hr>
-						<ul class="pricing-info">
-							<li>Internet de 15 MB</li>
-							<li>Coleção OI</li>
-							<li>Celular com Ligações Ilimitadas</li>
-							<li>8GB de Internet no Celular</li>
-						</ul>
-						<a href="#" class="btn btn-primary btn-sm">Aderir</a>
-					</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="gtco-section">
+		<div class="gtco-container">
+			<div class="row">
+				<div class="col-md-12 col-md-offset-0">
+					<ul class="fh5co-faq-list">
+						<li class="animate-box">
+							<h2 style="font-weight: 700; font-size: 35px; max-width: 450px">Oi Play: O melhor da TV quando você quiser</h2>
+							
+							
+							<div class="oiplay">
+
+								<div class="icons-oiplay">
+								<p>Além de todos os canais da sua Oi TV HD, a família toda pode assistir online a milhares de filmes, séries, desenhos, esportes e muito mais. E dá pra curtir tudo isso quando quiser, com total conforto, pelo computador, tablet ou celular. Você também pode aproveitar o Oi Play direto na sua TV.</p>
+									<img src="images/hbo-go.png">
+									<img src="images/logo-colecao-oi-oi-play.png">
+									<img src="images/crackle.png">
+									<img src="images/espn.png">
+									<img src="images/noggin.png">
+									<img src="images/discovery-kids.png">
+									<img src="images/fox-app-traco.png">	
+								</div>
+
+								<div style="max-width: 450px; margin: 0 40px">
+									<img style="width: 100%" src="images/oi-play_TV.png">
+								</div>
+							</div>
+						</li>
+
+						<div class="row" style="border-top: 3px solid #f1f1f1; padding: 30px 0">
+						  	<div class="col-sm-4">
+						  		<li class="animate-box">
+										<h2>Vai incluso</h2>
+								</li>
+						  	</div>
+						  	<div class="col-sm-8">
+									<li class="animate-box">
+										<h2>Modem WiFi: dentro de casa</h2>
+										<p>1 modem de internet WiFi cedido em regime de comodato. Perfeito para conectar seus aparelhos dentro de casa: computadores, tablets, celulares, TVs, videogames e mais</p>
+									</li>
+									<li class="animate-box">
+										<h2>Oi WiFi: fora de casa</h2>
+										<p>Mais de 2 milhões de pontos de internet pelo Brasil pra você navegar fora de casa em aeroportos, restaurantes e em muitos outros locais.</p>
+									</li>
+								</div>
+						</div>
+
+						<div class="row" style="border-top: 3px solid #f1f1f1; padding: 30px 0">
+						  	<div class="col-sm-4">
+						  		<li class="animate-box">
+										<h2>Você pode adicionar</h2>
+								</li>
+						  	</div>
+						  	<div class="col-sm-8">
+									<li class="animate-box">
+										<div class="row">
+											<div class="col-sm-8" style="border-bottom: 1px solid #f1f1f1"><h2>Internet Banda Larga 25 Mega</h2></div>
+											<div class="col-sm-4" style="border-bottom: 1px solid #f1f1f1"><h2>+10,00/mês</h2></div>
+										</div>
+									</li>
+									<li class="animate-box">
+										<div class="row">
+											<div class="col-sm-8" style="border-bottom: 1px solid #f1f1f1"><h2>Internet Banda Larga 35 Mega</h2></div>
+											<div class="col-sm-4" style="border-bottom: 1px solid #f1f1f1"><h2>+20,00/mês</h2></div>
+										</div>
+									</li>
+								</div>
+						</div>
+
+					</ul>
 				</div>
 			</div>
 		</div>
